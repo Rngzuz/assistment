12ef7676c775589bd940e9e196ea92ab24bab5f1
Tobias <cchawps@gmail.com>
2019-12-14 14:50:39 +0100

    Query to get min, max, and avg assignment_rating

--- queries.sql
+++ queries.sql
@@ -12,3 +12,8 @@ select min(problem_rating) min_rating, max(problem_rating) max_rating, avg(probl
  * get assignment_rating based on the avg. of problem_rating
  */
 select assignment_id, avg(problem_rating) assignment_rating from assignment join problem using (problem_id) group by assignment_id;
+
+/**
+ * get min., max., and avg. assignment_rating
+ */
+select min(assignment_rating) min_rating, max(assignment_rating) max_rating, avg(assignment_rating)::int avg_rating from (select avg(problem_rating)::int assignment_rating from assignment join problem using (problem_id) group by assignment_id) assignment_aggregate;
